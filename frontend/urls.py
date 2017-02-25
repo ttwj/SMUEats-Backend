3939c371e8d0188fc0a95de2a69097e4f7072819
Terence Tan <terencetanwj@gmail.com>
2017-02-25 17:17:51 +0800

    Templates working :)

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^order/$', views.list_merchants_index)
+    url(r'^order/$', views.list_merchants_index, name='frontend-order-index'),
+    url(r'^detail/(?P<merchant_id>\d+)/$', views.store_merchant_index, name='frontend-order-merchant-index'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 
-from api.models import Order, Merchant, MerchantLocation
+from api.models import Order, Merchant, MerchantLocation, MenuItem
 
 
 def index(request):
     context = {}
     return render(request, "index.html", context)
 
+
+def store_merchant_index(request, merchant_id):
+    merchant = Merchant.objects.get(id=merchant_id)
+    items = MenuItem.objects.filter(merchant=merchant)
+    return render(request, "order/store.html", {'merchant': merchant, 'items': items})
 
 def list_merchants_index(request):
     locations = MerchantLocation.objects.all()
@@ -19,6 +24,8 @@ def list_merchants_index(request):
     context = {
         'locations': merchant_dict
     }
+
+    print(context)
 
     return render(request, "order/index.html", context)
 
