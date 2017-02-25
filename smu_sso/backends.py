7ba16c64b965fe15e1fe3e3d91b25211e5cb6e2f
Nikos Chan <nik0sc@users.noreply.github.com>
2017-02-26 01:27:18 +0800

    sso auth backend less janky

--- smu_sso/backends.py
+++ smu_sso/backends.py
@@ -9,33 +9,33 @@ from smu_sso.models import SSOUser
 
 
 def sso_authenticate(username=None, details=None):
-    password = hashlib.md5(json.dumps(details, sort_keys=True).encode('utf-8')).hexdigest()
-    print("password " + password)
+    # md5 in 2017?????
+    # password = hashlib.md5(json.dumps(details, sort_keys=True).encode('utf-8')).hexdigest()
+    # print("password " + password)
     try:
         # Check if the user exists in Django's database
-        dict = json.loads(details)
+        user_details = json.loads(details)
         sso_user = SSOUser.objects.get(nric=username)
 
     except SSOUser.DoesNotExist:
         try:
-            #print(dict)
             user = User.objects.create(username=username, password=password,
-                                       first_name=dict['Account Holder Name(s):'])
+                                       first_name=user_details['Account Holder Name(s):'])
+            user.set_password(password)
 
 
-            sso_user = SSOUser.objects.create(account_holder_name=dict['Account Holder Name(s):'],
-                                              contact_number=dict['Contact Number(s):'],
-                                              bank_account_no=dict['Bank Account Number:'], user=user, nric=user)
+            sso_user = SSOUser.objects.create(account_holder_name=user_details['Account Holder Name(s):'],
+                                              contact_number=user_details['Contact Number(s):'],
+                                              bank_account_no=user_details['Bank Account Number:'], user=user, nric=user)
             return sso_user
         except Exception as e:
             logging.exception("Except 1")
             return None
 
-
-            # Check password of the user we found
-
+    # Check password of the user we found
     print("checking password " + password + " " + sso_user.user.password)
-    if password == sso_user.user.password:
+    #if password == sso_user.user.password:
+    if user.check_password(password):
         print("password correct!")
         return sso_user
     else:
