f5f06a56b97202a2280d5377b3ec3e105b950391
Terence Tan <terencetanwj@gmail.com>
2017-03-09 22:16:12 +0800

    Fix SSO

--- smueats_backend/settings.py
+++ smueats_backend/settings.py
@@ -167,5 +167,5 @@ LOGIN_REDIRECT_URL = '/frontend/'
 SSO_PUBLIC_KEY = 'KcJrHlft2tl36uMYMwt27mV1e3Iqr6alqhZA6SxvUiZFosXpp6HXRq4GNkrgZl18'
 SSO_PRIVATE_KEY = 'uRXh8TeZMR5LdaEaE8e3U7An66i6h4B4TuOVlrgavIaVyReZKvcdyDsGKvNirMPk'
 
-SSO_SERVER = 'http://beta.beepbeep.rocks/sso/'
+SSO_SERVER = 'https://beta.beepbeep.rocks/sso/'
 
